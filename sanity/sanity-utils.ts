cbd1f5793bb65150f690285eb46cf2de5a2d5f69
Christian Trujillo <christiantrujillo@MacBook-Pro-de-Christian-2.local>
2023-07-10 16:09:04 -0600

    import adjustment of project

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,60 +1,57 @@
-import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
-import clientConfig from "./config/client-config";
+import { Project } from "@/types/Project";
+import clientConfig from './config/client-config'
 import { Page } from "@/types/Page";
 
-export async function getProjects(): Promise<Project[]>{
-
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "project"]{
-            _id,
-            _createAt,
-            name,
-            "slug":slug.current,
-            "image": image.asset->url,
-            url,
-            content
-        }`
-    )
+export async function getProjects(): Promise<Project[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project"]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`
+  )
 }
 
-export async function getProject(slug: string): Promise<Project>{
-    
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "project" && slug.current == $slug][0]{
-            _id,
-            _createAt,
-            name,
-            "slug":slug.current,
-            "image": image.asset->url,
-            url,
-            content
-        }`,
-        {slug}
-    )
+export async function getProject(slug: string): Promise<Project> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+    }`,
+    { slug }
+  )
 }
 
 export async function getPages(): Promise<Page[]> {
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "page"]{
-            _id,
-            _createdAt,
-            title,
-            "slug": slug.current
-        }`
-    )
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current
+    }`
+  )
 }
 
-export async function getPage(slug:string): Promise<Page>{
-    return createClient(clientConfig).fetch(
-        groq`*[_type == "page" && slug.current == $slug][0]{
-            _id,
-            _cretedAt,
-            title,
-            "slug": slug.current,
-            content
-        }`,
-        { slug }
-    )
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+      content
+    }`,
+    { slug }
+  )
 }
-
