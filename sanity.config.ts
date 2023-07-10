5032200f52339d4bdc2dc627283cb4e92e48d9ab
Christian Trujillo <christiantrujillo@MacBook-Pro-de-Christian-2.local>
2023-07-10 15:18:01 -0600

    vision tool added

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "sanity";
 import {deskTool} from "sanity/desk";
 import schemas from "./sanity/schemas";
+import { visionTool } from "@sanity/vision";
 
 const config = defineConfig({
     projectId: "f5piaqz8",
@@ -8,7 +9,7 @@ const config = defineConfig({
     title: 'My Personal Website',
     apiVersion: "2023-07-03",
     basePath:"/admin",
-    plugins: [deskTool()],
+    plugins: [deskTool(), visionTool()],
     schema: {types: schemas}
 })
 
