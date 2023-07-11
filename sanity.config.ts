4538d89f04e7af4efa53ea939dbe142156c5fcb5
Christian Trujillo <christiantrujillo@MacBook-Pro-de-Christian-2.local>
2023-07-10 18:14:13 -0600

    vercel deploy

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from "sanity";
 import {deskTool} from "sanity/desk";
 import schemas from "./sanity/schemas";
 import { visionTool } from "@sanity/vision";
+import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 
 const config = defineConfig({
     projectId: "f5piaqz8",
@@ -9,7 +10,7 @@ const config = defineConfig({
     title: 'My Personal Website',
     apiVersion: "2023-07-03",
     basePath:"/admin",
-    plugins: [deskTool(), visionTool()],
+    plugins: [deskTool(), visionTool(), vercelDeployTool()],
     schema: {types: schemas}
 })
 
